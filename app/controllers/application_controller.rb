c55272edfae7aa4a2151aaa477391a387307c1f9
Geordee Naliyath <geordee@gmail.com>
2014-03-19 21:44:22 +0530

    Change the layout of login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'new'
+      "login"
+    else
+      "application"
+    end
+  end
 end
